5a72ff033b8123c80a3eef616100cce38136f49e
aaronlin79 <aaron.lin792@gmail.com>
2023-11-20 17:48:03 -0800

    Created quiz input driver to take inputs for the quiz and return a vector of char

--- src/Input.cpp
+++ src/Input.cpp
@@ -16,4 +16,48 @@ string Input::getSimilar() {}
 
 vector<string> Input::getFilter() {}
 
-vector<char> Input::getQuiz() {}
+vector<char> Input::getQuiz() {
+    vector<char> quizInputs;
+
+    questionOut1();
+    char questionInput1;
+    do {
+        cin >> questionInput1;
+    }
+    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    quizInputs.at(0) = questionInput1;
+
+    questionOut2();
+    char questionInput2;
+    do {
+        cin >> questionInput2;
+    }
+    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    quizInputs.at(1) = questionInput2;
+
+    questionOut3();
+    char questionInput3;
+    do {
+        cin >> questionInput3;
+    }
+    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    quizInputs.at(2) = questionInput3;
+
+    questionOut4();
+    char questionInput4;
+    do {
+        cin >> questionInput4;
+    }
+    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    quizInputs.at(3) = questionInput4;
+
+    questionOut5();
+    char questionInput5;
+    do {
+        cin >> questionInput5;
+    }
+    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    quizInputs.at(4) = questionInput5;
+
+    return quizInputs;
+}
--- test/testOut.cpp
+++ test/testOut.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <cstdlib>
-#include "../header/Output.h"
+#include <vector>
+#include <string>
+#include "../header/Input.h"
 
 using namespace std;
 
 int main() {
+    Input *o = new Input();
+    o->getQuiz();
+    delete o;
+
     return 0;
 }
