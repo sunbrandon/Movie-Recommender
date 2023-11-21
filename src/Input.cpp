bfd7ff401803ae03e46845b8987768efde93fb76
davidbjol <davidbjolee@gmail.com>
2023-11-21 15:46:30 -0800

    Added getFilter() to Input.cpp

--- src/Input.cpp
+++ src/Input.cpp
@@ -39,7 +39,65 @@ char Input::getSearch() {}
 
 string Input::getSimilar() {}
 
-vector<string> Input::getFilter() {}
+vector<string> Input::getFilter() {
+    vector<string> filterInputs;
+
+    string genreInput;
+    string yearInput;
+    string durationInput;
+    string ratingInput;
+    string popularityInput;
+
+    filterOutGenre();
+    do { 
+        // WOULD THIS WORK?
+        // if (genreInput >= "1" && genreInput <= "20") {
+        //     cout << "Invalid input, please try again." << endl;  
+        // }
+        cin >> genreInput;
+    }
+    while (genreInput != "1" & genreInput != "2" & genreInput != "3" & genreInput != "4" & genreInput != "5" & 
+            genreInput != "6" & genreInput != "7" & genreInput != "8" & genreInput != "9" & genreInput != "10" & 
+            genreInput != "11" & genreInput != "12" & genreInput != "13" & genreInput != "14" & genreInput != "15" & 
+            genreInput != "16" & genreInput != "17" & genreInput != "18" & genreInput != "19" & genreInput != "20"); 
+    
+    filterInputs.push_back(genreInput);
+    
+    filterOutYear();
+    do {
+        cin >> yearInput;
+    }
+    while (yearInput != "a" & yearInput != "b" & yearInput != "c" & yearInput != "d" & yearInput != "e");
+
+    filterInputs.push_back(yearInput);
+
+    filterOutDuration();
+    do {
+        cin >> durationInput;
+    }
+    while (durationInput != "a" & durationInput != "b" & durationInput != "c" & durationInput != "d" & durationInput != "e" & durationInput != "f");
+
+    filterInputs.push(durationInput);
+    
+    filterOutRating();
+    do {
+        cin >> ratingInput;
+    }
+    while (ratingInput != "a" & ratingInput != "b" & ratingInput != "c" & ratingInput != "d" & ratingInput != "e" & ratingInput != "f" & ratingInput != "g");
+
+    filterInput.push(ratingInput);
+
+    filterOutPopularity() {
+    do {
+        cin >> popularityInput;
+    }
+    while (popularityInput != "a" & popularityInput != "b" & popularityInput != "c" & popularityInput != "d" & popularityInput != "e");
+
+    filterInputs.push(popularityInput);
+
+    return filterInputs;
+    }
+}
 
 vector<char> Input::getQuiz() {
     vector<char> quizInputs;
