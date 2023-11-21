9c8584d2775df0ee1c1fe3330344912dcbc6061c
davidbjol <davidbjolee@gmail.com>
2023-11-20 17:42:05 -0800

    Updated input.cpp for getMenu()

--- src/Input.cpp
+++ src/Input.cpp
@@ -1,6 +1,8 @@
 #include "../header/Input.h"
+#include "../header/Output.h"
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
@@ -8,7 +10,30 @@ Input::Input() {}
 
 Input::~Input() {}
 
-char Input::getMenu() {}
+char Input::getMenu() {
+    char input;
+    outputMenu();
+        
+    cin >> input;
+    while(input != '1' || input != '2' || input != '3' || input != 'q') {
+        cout << "Invalid input, please try again." << endl;
+        cin >> input;
+    }
+
+    if (input == '1') {
+        cout << "IMPLEMENT: getSearch();" << endl;
+    }
+    else if (input == '2') {
+        cout << "IMPLEMENT: getQuiz();" << endl;
+    }
+    else if (input == '3') {
+        cout << "IMPLEMENT: RANDOM" << endl;
+    }
+    else if (input == 'q') {
+        cout << "IMPLEMENT: EXIT" << endl;
+    }
+    
+}
 
 char Input::getSearch() {}
 
