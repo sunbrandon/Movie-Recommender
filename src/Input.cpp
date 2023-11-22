ecd56c85118feda05d334b4af215cc1a9824dcf4
aaronlin79 <aaron.lin792@gmail.com>
2023-11-21 16:47:46 -0800

    Fixed do while loop bug, where wrong variable was used

--- src/Input.cpp
+++ src/Input.cpp
@@ -18,45 +18,45 @@ vector<string> Input::getFilter() {}
 
 vector<char> Input::getQuiz() {
     vector<char> quizInputs;
+    char questionInput1;
+    char questionInput2;
+    char questionInput3;
+    char questionInput4;
+    char questionInput5;
 
     questionOut1();
-    char questionInput1;
     do {
         cin >> questionInput1;
     }
-    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    while (questionInput1 != 'a' & questionInput1 != 'b' & questionInput1 != 'c' & questionInput1 != 'd');
     quizInputs.at(0) = questionInput1;
 
     questionOut2();
-    char questionInput2;
     do {
         cin >> questionInput2;
     }
-    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    while (questionInput2 != 'a' & questionInput2 != 'b' & questionInput2 != 'c' & questionInput2 != 'd');
     quizInputs.at(1) = questionInput2;
 
     questionOut3();
-    char questionInput3;
     do {
         cin >> questionInput3;
     }
-    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    while (questionInput3 != 'a' & questionInput3 != 'b' & questionInput3 != 'c' & questionInput3 != 'd');
     quizInputs.at(2) = questionInput3;
 
     questionOut4();
-    char questionInput4;
     do {
         cin >> questionInput4;
     }
-    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    while (questionInput4 != 'a' & questionInput4 != 'b' & questionInput4 != 'c' & questionInput4 != 'd');
     quizInputs.at(3) = questionInput4;
 
     questionOut5();
-    char questionInput5;
     do {
         cin >> questionInput5;
     }
-    while (input != 'a' & input != 'b' & input != 'c' & input != 'd');
+    while (questionInput5 != 'a' & questionInput5 != 'b' & questionInput5 != 'c' & questionInput5 != 'd');
     quizInputs.at(4) = questionInput5;
 
     return quizInputs;
