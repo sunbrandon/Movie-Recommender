d53bccf6055874b6209e0ecc4ff2e9553b4491df
github-username <dbanh007@ucr.edu>
2023-12-02 16:07:10 -0800

    Added quiz point system

--- header/Quiz.h
+++ header/Quiz.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 #include "Input.h"
 #include "Movie.h"
 
@@ -12,9 +13,15 @@ class Quiz : public Input, public Sort {
     Quiz();
     ~Quiz();
     vector<Movie> quizAlgo();
+    void q1Points(char choice);
+    void q2Points(char choice);
+    void q3Points(char choice);
+    void q4Points(char choice);
+    void q5Points(char choice);
   
  private:
 
     Quiz operator=(const Output&) = delete;
+    vector <unsigned int> points;
 
 };
