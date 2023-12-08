48433437b4c0f64a36ca2c432f7c273a5b7ef4ef
Brandon Sun <132787942+sunbrandon@users.noreply.github.com>
2023-12-08 01:40:28 -0800

    Update testOut.cpp

--- test/testOut.cpp
+++ test/testOut.cpp
@@ -10,7 +10,6 @@
 #include "../header/Result.h"
 #include "../header/Search.h"
 #include "../header/Sort.h"
-#include "../header/Tests.h"
 
 using namespace std;
 
@@ -27,52 +26,6 @@ int main() {
 
     delete s;
     delete in;
-
-    // Tests *t = new Tests();
-
-    // cout << "MOVIE TESTS: " << endl;
-    // cout << endl;
-
-    // cout << t->setYearTest() << endl;
-    // cout << t->setNumOfVotesTest() << endl;
-    // cout << t->setDurationTest() << endl;
-
-    // cout << t->setRatingTest() << endl;
-
-    // cout << t->setTitleTest() << endl;
-    // cout << t->setProductionTest() << endl;
-    // cout << t->setMovieIDTest() << endl;
-
-    // cout << t->setGenresTest() << endl;
-    // cout << t->setDirectorsTest() << endl;
-    // cout << t->setWritersTest() << endl;
-    // cout << t->setActorsTest() << endl;
-    // cout << t->setDescriptionsTest() << endl;
-
-    // cout << t->getYearTest() << endl;
-    // cout << t->getNumOfVotesTest() << endl;
-    // cout << t->getDurationTest() << endl;
-
-    // cout << t->getRatingTest() << endl;
-
-    // cout << t->getTitleTest() << endl;
-    // cout << t->getProductionTest() << endl;
-    // cout << t->getMovieIDTest() << endl;
-
-    // cout << t->getGenresTest() << endl;
-    // cout << t->getDirectorsTest() << endl;
-    // cout << t->getWritersTest() << endl;
-    // cout << t->getActorsTest() << endl;
-    // cout << t->getDescriptionsTest() << endl;
-
-    // cout << endl;
-
-    // cout << "SORT TESTS: " << endl;
-    // cout << endl;
-
-    // cout << t->sortCSVTest() << endl;
-
-    // delete t;
 
     return 0;
 }
